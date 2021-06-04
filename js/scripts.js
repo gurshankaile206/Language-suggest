43b5fa89cb0ea19189587fe7fe99d4e3f91d721f
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-04 13:51:33 -0700

    add second question

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-$("form#currentPlace").submit(function(event) { 
+$("form#survey").submit(function(event) { 
     const city = $("input:radio[name=place]:checked").val();
     if (city === "Portland") {
         $("#ruby").show();
     } else if (city === "Seattle") {
         $("#c").show();
-    } else if (city === "SomewhereElse") {
-        $("#javscript").show();
+    } else if (city === "Other") {
+        $("#j").show();
     } else {
         alert("No Value Selected!");
     }

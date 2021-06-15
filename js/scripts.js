4c7ca6b54860a12e36b7bdf2df9b9e6d8c38b1f9
Gurshan Kaile <gurshankaile206@gmail.com>
2021-06-15 01:44:08 -0700

    fix indentation using 2 spaces and indentation rainbow extensions

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 $("form#survey").submit(function(event) { 
-    event.preventDefault();
-    const city = $("input:radio[name=place]:checked").val();
-    if (city === "Portland") {
-        $("#ruby").show();
-        $("#c").hide();
-        $('#j').hide();
+  event.preventDefault();
+  const city = $("input:radio[name=place]:checked").val();
+  if (city === "Portland") {
+    $("#ruby").show();
+    $("#c").hide();
+    $('#j').hide();
     } else if (city === "Seattle") {
-        $("#c").show();
-        $("#ruby").hide();
-        $("#j").hide();
+      $("#c").show();
+      $("#ruby").hide();
+      $("#j").hide();
     } else if (city === "Other") {
-        $("#j").show();
-        $("#ruby").hide();
-        $("#c").hide();
+      $("#j").show();
+      $("#ruby").hide();
+      $("#c").hide();
     } else {
-        alert("No Value Selected!");
+      alert("No Value Selected!");
     }
 
         
